241f09be944f15e51cdcb08e29f4904e2a23dd78
Priyansu Tank <pbtank@users.noreply.github.com>
2026-01-26 15:36:29 +0530

    Test 10c

--- sketch.js
+++ sketch.js
@@ -105,9 +105,9 @@ function draw() {
 
   // updateOrientation();
 
-  // if (worldMatrix) {
-  //   applyWorldTransform();
-  // }
+  if (worldMatrix) {
+    applyWorldTransform();
+  }
   if (gravity) {
     // Draw world-locked content
     drawWorldLockedArrow();
@@ -164,7 +164,7 @@ function draw() {
   // hud text
   hud.clear();
   hud.fill(255, 0, 0);
-  hud.text("Test 10b", 10, 30);
+  hud.text("Test 10c", 10, 30);
   if (gravity) {
     hud.text("g : " + gravity.x.toFixed(3) + ", " + gravity.y.toFixed(3) + ", " + gravity.z.toFixed(3), 50, 50);
   }
